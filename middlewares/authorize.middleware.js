6664970c08e32037a4bf1ba9265b1d37c8a616ea
Mohammedal-nabahin <moha.nabahin@gmail.com>
2024-12-30 15:36:58 +0200

    Add  middlewares

---
+++ middlewares/authorize.middleware.js
@@ -0,0 +1,15 @@
+const authorizeRole = (allowedRoles) => {
+    return (req, res, next) => {
+      const userRole = req.user?.role;
+  
+      if (!userRole || !allowedRoles.includes(userRole)) {
+        return res.status(403).json({
+          message: `Access forbidden: You must be one of the following roles: ${allowedRoles.join(", ")}.`,
+        });
+      }
+      next();
+    };
+  };
+  
+  module.exports = authorizeRole;
+  
