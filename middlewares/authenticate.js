b9674cb9a074ae99180d001106e5891eaff349bf
Mohammedal-nabahin <moha.nabahin@gmail.com>
2024-12-31 16:10:03 +0200

    Edit Middlewares

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -4,7 +4,7 @@ const SECRET_KEY = process.env.SECRET_KEY;
 const authenticate = (req, res, next) => {
   const token = req.headers.authorization?.split(" ")[1];
   if (!token) return res.status(401).json({ message: "Unauthorized: Token missing" });
-
+  
   try {
     const decoded = jwt.verify(token, SECRET_KEY);
     req.user = decoded;
