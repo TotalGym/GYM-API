52a7b59cef846b404e5e4a44ad64fe816fac833d
Mohammedal-nabahin <moha.nabahin@gmail.com>
2025-01-02 15:56:59 +0200

    Fix staff route error  & Edit trainee route

--- routes/staff.route.js
+++ routes/staff.route.js
@@ -3,12 +3,12 @@ const router = express.Router();
 const staffController = require("../controllers/staff.controller.js");
 const authorizeRole = require("../middlewares/authorize.middleware.js");
 
-router.get("/", authorizeRole(["Admin", "superAdmin"]), staffController.getAllStaff);
-router.get("/:id", authorizeRole(["superAdmin", "Admin", "Staff"]), staffController.getStaffById);
+router.get("/", authorizeRole(["Admin", "SuperAdmin"]), staffController.getAllStaff);
+router.get("/:id", authorizeRole(["SuperAdmin", "Admin", "Staff"]), staffController.getStaffById);
 
-router.post("/", authorizeRole(["superAdmin", "Admin"]), staffController.addStaff);
-router.put("/:id", authorizeRole(["superAdmin", "Admin", "Staff"]), staffController.updateStaff);
+router.post("/", authorizeRole(["SuperAdmin", "Admin"]), staffController.addStaff);
+router.put("/:id", authorizeRole(["SuperAdmin", "Admin", "Staff"]), staffController.updateStaff);
 
-router.delete("/:id", authorizeRole(["superAdmin", "Admin"]), staffController.deleteStaff);
+router.delete("/:id", authorizeRole(["SuperAdmin", "Admin"]), staffController.deleteStaff);
 
 module.exports = router;
--- routes/trainee.route.js
+++ routes/trainee.route.js
@@ -1,15 +1,18 @@
 const express = require("express");
 const { createTrainee, getTrainees, getTraineeById, updateTrainee, deleteTrainee, changePassword } = require("../controllers/trainee.controller.js");
-const authenticate = require("../middlewares/authenticate");
+const {authenticate} = require("../middlewares/authenticate");
 const authorizeRole = require("../middlewares/authorize.middleware.js");
+const { paginatedResults } = require("../middlewares/pagination.js");
+const traineeModel = require("../models/trainee.model.js");
+
 
 const router = express.Router();
 
 router.post("/", createTrainee);
-router.get("/", getTrainees);
+router.get("/", paginatedResults(traineeModel), getTrainees);
 router.get("/:id", getTraineeById);
-router.put("/:id", updateTrainee); //note that the admin can't change all the data ?? 
-router.put("/:id/changePassword", authorizeRole(["Trainee"]) , changePassword);
+router.put("/:id", updateTrainee); //Admin can't change all the data ? 
+router.put("/changePassword/:id", authenticate, authorizeRole(["Trainee"]) , changePassword);
 router.delete("/:id", deleteTrainee);
 
 module.exports = router;
