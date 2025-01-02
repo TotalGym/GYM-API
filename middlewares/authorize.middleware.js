441556d781f8662dc754e9f9deeda628b4503602
Mohammedal-nabahin <moha.nabahin@gmail.com>
2025-01-02 15:51:25 +0200

    Add pagination middleware

--- middlewares/authorize.middleware.js
+++ middlewares/authorize.middleware.js
@@ -1,7 +1,7 @@
-const authorizeRole = (allowedRoles) => {
+ const authorizeRole = (allowedRoles) => {
     return (req, res, next) => {
       const userRole = req.user?.role;
-      console.log(userRole)
+  
       if (!userRole || !allowedRoles.includes(userRole)) {
         return res.status(403).json({
           message: `Access forbidden: You must be one of the following roles: ${allowedRoles.join(", ")}.`,
