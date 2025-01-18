f2e168f05a434c613b2af44767294e19daa85bc8
Mohammedal-nabahin <moha.nabahin@gmail.com>
2025-01-18 14:07:10 +0200

    Check validation

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,17 +1,16 @@
 const express = require("express");
 const {login, changePassword, forgotPassword, verifyResetCode, resetPassword} = require("../controllers/auth/auth.controller.js");
 const { authenticate } = require("../middlewares/authenticate.js");
-const validate = require("../middlewares/validate.middleware.js");
 const { loginValidation, changePasswordValidation, forgotPasswordValidation, verifyResetCodeValidation, resetPasswordValidation } = require("../utils/validators/auth.validator.js");
 
 const router = express.Router();
 
 
-router.post("/login", validate(loginValidation) ,login);
-router.put("/changePassword/:id", validate(changePasswordValidation), authenticate, changePassword);
-router.post("/forgot-password", validate(forgotPasswordValidation), forgotPassword);
-router.post("/verify",validate(verifyResetCodeValidation), verifyResetCode);
-router.put("/reset-password",validate(resetPasswordValidation), resetPassword);
+router.post("/login", loginValidation ,login);
+router.put("/changePassword/:id", changePasswordValidation, authenticate, changePassword);
+router.post("/forgot-password", forgotPasswordValidation, forgotPassword);
+router.post("/verify", verifyResetCodeValidation, verifyResetCode);
+router.put("/reset-password", resetPasswordValidation, resetPassword);
 
 
 module.exports = router;
