7b8d6050b19fdba05751c68f9c514deab19d394d
Mohammedal-nabahin <moha.nabahin@gmail.com>
2025-02-01 14:36:49 +0200

    Small edit

--- controllers/auth/auth.controller.js
+++ controllers/auth/auth.controller.js
@@ -44,14 +44,12 @@ exports.login = async (req, res) => {
    res.cookie("accessToken", accessToken, {
       httpOnly: true,
       secure: true,
-      sameSite: "Strict",
       maxAge: 15 * 60 * 1000,
     });
 
     res.cookie("refreshToken", refreshToken, {
       httpOnly: true,
       secure: true,
-      sameSite: "Strict",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
