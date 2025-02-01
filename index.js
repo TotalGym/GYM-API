e19fc5a1ee25b3797ff92026ff626e033723356f
Mohammedal-nabahin <moha.nabahin@gmail.com>
2025-02-01 15:26:14 +0200

    Edit cors

--- index.js
+++ index.js
@@ -21,20 +21,7 @@ const allowedOrigins = [
 ]
 
 const app = express();
-app.use(cors({
-    origin: function (origin, callback) {
-        if (!origin) return callback(null, true);
-  
-        if (allowedOrigins.includes(origin)) {
-          callback(null, true);
-        } else {
-          callback(new Error('Not allowed by CORS'));
-        }
-      },
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-}));
+app.use(cors());
 app.options("*" , cors());
 
 app.use(cookieParser()); 
