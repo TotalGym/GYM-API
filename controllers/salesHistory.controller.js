48c000d1ffd557a0577e39f730c60b6b5483bee3
MohammedAl-Nabahin <moha.nabahin@gmail.com>
2025-02-27 14:09:31 +0200

    Update sales history retrieving

--- controllers/salesHistory.controller.js
+++ controllers/salesHistory.controller.js
@@ -1,13 +1,19 @@
-const SalesHistory = require('../models/salesHistory.model.js');
-const Store = require('../models/store.model.js');
-const { paginatedResults } = require('../utils/pagination.js');
-const { responseHandler } = require('../utils/responseHandler.js');
-const { search } = require('../utils/search.js');
+const SalesHistory = require("../models/salesHistory.model.js");
+const Store = require("../models/store.model.js");
+const { paginatedResults } = require("../utils/pagination.js");
+const { responseHandler } = require("../utils/responseHandler.js");
+const { search } = require("../utils/search.js");
 
 exports.Sell = async (req, res) => {
   try {
     const { ProductID, TraineeID, quantitySold } = req.body;
-    if (!ProductID || !quantitySold || !TraineeID || isNaN(quantitySold) || quantitySold <= 0) {
+    if (
+      !ProductID ||
+      !quantitySold ||
+      !TraineeID ||
+      isNaN(quantitySold) ||
+      quantitySold <= 0
+    ) {
       return responseHandler(res, 400, false, "Invalid input or missing");
     }
 
@@ -30,19 +36,28 @@ exports.Sell = async (req, res) => {
       ProductID,
       quantitySold,
       totalSaleValue,
-      TraineeID
+      TraineeID,
     });
 
     const productData = {
       name: product.productName,
       quantity: product.inventoryCount,
-      totalRevenue: product.totalRevenue 
+      totalRevenue: product.totalRevenue,
     };
 
-    return responseHandler(res, 201, true, "Sale recorded successfully", { sale, productData });
-
+    return responseHandler(res, 201, true, "Sale recorded successfully", {
+      sale,
+      productData,
+    });
   } catch (error) {
-    return responseHandler(res, 500, false, "Failed to record sale", null, error.message);
+    return responseHandler(
+      res,
+      500,
+      false,
+      "Failed to record sale",
+      null,
+      error.message
+    );
   }
 };
 //todo: collect products being sold the most in each month for analytics
@@ -50,80 +65,133 @@ exports.Sell = async (req, res) => {
 exports.getAllSales = async (req, res) => {
   try {
     const { searchTerm } = req.query;
-    const searchQuery = search(SalesHistory, searchTerm);
+    const searchQuery = {
+      ...search(SalesHistory, searchTerm),
+      ProductID: { $ne: null },
+    };
 
-  
     const options = {
       populateFields: [
-        { path: "ProductID", select:"productName" },
-        { path: "TraineeID", select: "name email" }
+        { path: "ProductID", select: "productName" },
+        { path: "TraineeID", select: "name email" },
       ],
     };
 
-    const paginatedData = await paginatedResults(SalesHistory, searchQuery, req, options);
+    const paginatedData = await paginatedResults(
+      SalesHistory,
+      searchQuery,
+      req,
+      options
+    );
 
-    // paginatedData.results = paginatedData.results.filter(sale => sale.ProductID);
+    paginatedData.results = paginatedData.results.filter(
+      (sale) => sale.ProductID
+    );
 
-    responseHandler(res, 200, true, "Sales history retrieved successfully", paginatedData);
+    responseHandler(
+      res,
+      200,
+      true,
+      "Sales history retrieved successfully",
+      paginatedData
+    );
   } catch (error) {
-    responseHandler(res, 500, false, `Error fetching sales`, null, error.message);
+    responseHandler(
+      res,
+      500,
+      false,
+      `Error fetching sales`,
+      null,
+      error.message
+    );
   }
 };
-
 
 exports.getSalesByProduct = async (req, res) => {
   const { productId } = req.params;
 
   try {
-    const sales = await SalesHistory.find({ ProductID: productId }).populate('ProductID', 'ProductName');
-    responseHandler(res, 200, true, "Sales history for product retrieved successfully", sales);
+    const sales = await SalesHistory.find({ ProductID: productId }).populate(
+      "ProductID",
+      "ProductName"
+    );
+    responseHandler(
+      res,
+      200,
+      true,
+      "Sales history for product retrieved successfully",
+      sales
+    );
   } catch (error) {
-    responseHandler(res, 500, false, "Failed to retrieve sales", null, error.message);
+    responseHandler(
+      res,
+      500,
+      false,
+      "Failed to retrieve sales",
+      null,
+      error.message
+    );
   }
 };
-
 
 exports.deleteSale = async (req, res) => {
   const { id } = req.params;
 
   try {
     const sale = await SalesHistory.findByIdAndDelete(id);
-    if (!sale) return responseHandler(res, 404, false, "Sale history not found");
+    if (!sale)
+      return responseHandler(res, 404, false, "Sale history not found");
 
     responseHandler(res, 200, true, "Sale history deleted successfully");
   } catch (error) {
-    responseHandler(res, 500, false, "Failed to delete sale history", null, error.message);
+    responseHandler(
+      res,
+      500,
+      false,
+      "Failed to delete sale history",
+      null,
+      error.message
+    );
   }
 };
 
-
 exports.getFilteredSales = async (req, res) => {
-    try {
-      const sales = await SalesHistory.find()
-        .populate({
-          path: 'ProductID',
-          select: 'ProductName',
-        })
-        .populate({
-          path: 'TraineeID',
-          select: 'Name Contact.Email',
-        })
-        ;
-  
-      const formattedSales = sales.map((sale) => ({
-        id: sale._id,
-        productName: sale.ProductID?.ProductName || null,
-        traineeName: sale.TraineeID?.Name || null,
-        traineeEmail: sale.TraineeID?.Contact?.Email || null,
-        quantitySold: sale.QuantitySold,
-        price: sale.Price,
-        totalSaleValue: sale.TotalSaleValue,
-        saleDate: sale.SaleDate,
-      }));
-  
-      responseHandler(res, 200, true, "Filtered sales retrieved successfully", formattedSales);
-    } catch (error) {
-      responseHandler(res, 500, false, "Failed to retrieve filtered sales", null, error.message);
-    }
-  };
-  
+  try {
+    const sales = await SalesHistory.find()
+      .populate({
+        path: "ProductID",
+        select: "ProductName",
+      })
+      .populate({
+        path: "TraineeID",
+        select: "Name Contact.Email",
+      });
+    const formattedSales = sales.map((sale) => ({
+      id: sale._id,
+      productName: sale.ProductID?.ProductName || null,
+      traineeName: sale.TraineeID?.Name || null,
+      traineeEmail: sale.TraineeID?.Contact?.Email || null,
+      quantitySold: sale.QuantitySold,
+      price: sale.Price,
+      totalSaleValue: sale.TotalSaleValue,
+      saleDate: sale.SaleDate,
+    }));
+
+    responseHandler(
+      res,
+      200,
+      true,
+      "Filtered sales retrieved successfully",
+      formattedSales
+    );
+  } catch (error) {
+    responseHandler(
+      res,
+      500,
+      false,
+      "Failed to retrieve filtered sales",
+      null,
+      error.message
+    );
+  }
+};
--- routes/salesHistory.route.js
+++ routes/salesHistory.route.js
@@ -1,11 +1,10 @@
-const express = require('express');
-const salesController = require('../controllers/salesHistory.controller.js');
+const express = require("express");
+const salesController = require("../controllers/salesHistory.controller.js");
 const router = express.Router();
 
-router.post('/', salesController.Sell);
-router.get('/', salesController.getAllSales);
-router.get('/:productId', salesController.getSalesByProduct);
-router.get('/', salesController.getFilteredSales);
-router.delete('/:id', salesController.deleteSale);
+router.post("/", salesController.Sell);
+router.get("/", salesController.getAllSales);
+router.get("/:productId", salesController.getSalesByProduct);
+router.delete("/:id", salesController.deleteSale);
 
 module.exports = router;
