eb4dcb72f8eabde13d05523bde22b89329ea9cbd
Mohammedal-nabahin <moha.nabahin@gmail.com>
2025-03-24 23:44:05 +0200

    Add PATCH to cors methods

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-const express = require('express');
+const express = require("express");
 const dbConnection = require("./config/database");
 const dotenv = require("dotenv");
-const cors = require('cors');
+const cors = require("cors");
 
 const errorHandler = require("./middlewares/error.middleware.js");
 
-const dashboardRoutes = require('./utils/split-routes/dashboard.routes.js');
-const appRoutes = require('./utils/split-routes/app.routes.js');
-
+const dashboardRoutes = require("./utils/split-routes/dashboard.routes.js");
+const appRoutes = require("./utils/split-routes/app.routes.js");
 
 dotenv.config();
 
@@ -17,41 +16,40 @@ dbConnection();
 const app = express();
 
 const allowedOrigins = [
-    process.env.PRODUCTION_BUILD,
-    process.env.DEVELOPMENT_BUILD,
+  process.env.PRODUCTION_BUILD,
+  process.env.DEVELOPMENT_BUILD,
 ];
 
-
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-}));
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 const PORT = process.env.PORT || 3000;
 
-
 app.use(express.json());
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 
-app.get('/', (req, res)=> {
-res.send("Welcome to GYM API!")
+app.get("/", (req, res) => {
+  res.send("Welcome to GYM API!");
 });
 
 app.use("/api/dashboard", dashboardRoutes);
 app.use("/api/app", appRoutes);
 
-
 app.use(errorHandler);
 
-app.get('*', (req, res)=> {
-    res.status(404).send("Page Not Found");
+app.get("*", (req, res) => {
+  res.status(404).send("Page Not Found");
 });
 
 app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
